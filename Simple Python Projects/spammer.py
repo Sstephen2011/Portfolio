f52a9f0eb239718192408174eba78c10184e9925
Stephen Peter S <stephenps2011@gmail.com>
2025-08-22 21:02:02 +0530

    Create spammer.py

---
+++ Simple Python Projects/spammer.py
@@ -0,0 +1,61 @@
+import pyautogui, time, datetime, random
+  
+time.sleep(2) 
+  
+
+
+while True: 
+    message_chooser = random.randint(1, 10)
+    if message_chooser == 1:
+        print(datetime.datetime.now()) 
+        pyautogui.typewrite("Appear weak when you are strong, and strong when you are weak. \n ― Sun Tzu")  
+        pyautogui.press("enter") 
+        time.sleep(3) 
+    elif message_chooser == 2:
+        print(datetime.datetime.now()) 
+        pyautogui.typewrite("The supreme art of war is to subdue the enemy without fighting. \n ― Sun Tzu")  
+        pyautogui.press("enter") 
+        time.sleep(3) 
+    elif message_chooser == 3:
+        print(datetime.datetime.now()) 
+        pyautogui.typewrite("If you know the enemy and know yourself, you need not fear the result of a hundred battles. \n -Sun Tzu")  
+        pyautogui.press("enter") 
+        time.sleep(3) 
+    elif message_chooser == 4:
+        print(datetime.datetime.now()) 
+        pyautogui.typewrite("In the midst of chaos, there is also opportunity. \n -Sun Tzu")  
+        pyautogui.press("enter") 
+        time.sleep(3) 
+    elif message_chooser == 5:
+        print(datetime.datetime.now()) 
+        pyautogui.typewrite("Let your plans be dark and impenetrable as night, and when you move, fall like a thunderbolt. \n -Sun Tzu") 
+        pyautogui.press("enter") 
+        time.sleep(3) 
+    elif message_chooser == 6:
+        print(datetime.datetime.now()) 
+        pyautogui.typewrite("All warfare is based on deception. \n -Sun Tzu")  
+        pyautogui.press("enter") 
+        time.sleep(3) 
+    elif message_chooser == 7:
+        print(datetime.datetime.now()) 
+        pyautogui.typewrite("The greatest victory is that which requires no battle. \n -Sun Tzu") 
+        pyautogui.press("enter") 
+        time.sleep(3) 
+    elif message_chooser == 8:
+        print(datetime.datetime.now()) 
+        pyautogui.typewrite("There is no instance of a nation benefitting from prolonged warfare. \n -Sun Tzu") 
+        pyautogui.press("enter") 
+        time.sleep(3) 
+    elif message_chooser == 9:
+        print(datetime.datetime.now()) 
+        pyautogui.typewrite("If your enemy is secure at all points, be prepared for him. If he is in superior strength, evade him. \n -Sun Tzu")
+        pyautogui.press("enter") 
+        time.sleep(3) 
+    elif message_chooser == 10:
+        print(datetime.datetime.now()) 
+        pyautogui.typewrite("To know your Enemy, you must become your Enemy. \n -Sun Tzu")  
+        pyautogui.press("enter") 
+        time.sleep(3) 
+    
+  
+  
